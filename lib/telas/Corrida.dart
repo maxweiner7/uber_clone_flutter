e5024028a671eeeb448fc9e0b7db025e497e7416
max.wener@hotmail.com <max.wener@hotmail.com>
2020-12-16 22:43:16 +0000

    Finalizar corrida e calcular preço

--- lib/telas/Corrida.dart
+++ lib/telas/Corrida.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intl/intl.dart';
 import 'package:uber/model/Usuario.dart';
 import 'package:uber/util/StatusRequisicao.dart';
 import 'package:uber/util/UsuarioFirebase.dart';
@@ -57,7 +58,6 @@ class _CorridaState extends State<Corrida> {
 
         if (_idRequisicao != null && _idRequisicao.isNotEmpty) {
           if (_statusRequisicao != StatusRequisicao.AGUARDANDO) {
-
             //Atualiza local do passageiro
             UsuarioFirebase.atualizarDadosLocalizacao(
                 _idRequisicao, position.latitude, position.longitude);
@@ -140,6 +140,7 @@ class _CorridaState extends State<Corrida> {
             _statusEmViagem();
             break;
           case StatusRequisicao.FINALIZADA:
+            _statusFinalizada();
             break;
         }
       }
@@ -211,6 +212,61 @@ class _CorridaState extends State<Corrida> {
     ));
   }
   _finalizarCorrida() {
+
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    db.collection("requisicoes")
+    .doc( _idRequisicao )
+    .update( {
+      "status" : StatusRequisicao.FINALIZADA
+    });
+
+    String idPassageiro = _dadosRequisicao["passageiro"]["idUsuario"];
+    db
+        .collection("requisicao_ativa")
+        .doc(idPassageiro)
+        .update({"status": StatusRequisicao.FINALIZADA});
+
+    String idMotorista = _dadosRequisicao["motorista"]["idUsuario"];
+    db
+        .collection("requisicao_ativa_motorista")
+        .doc(idMotorista)
+        .update({"status": StatusRequisicao.FINALIZADA});
+
+  }
+  _statusFinalizada() async {
+
+    //Calcula valor da corrida
+    double latitudeDestino = _dadosRequisicao["destino"]["latitude"];
+    double longitudeDestino = _dadosRequisicao["destino"]["longitude"];
+
+    double latitudeOrigem = _dadosRequisicao["origem"]["latitude"];
+    double longitudeOrigem = _dadosRequisicao["origem"]["longitude"];
+    
+    double distanciaEmMetros = await Geolocator.distanceBetween(
+        latitudeOrigem,
+        longitudeOrigem,
+        latitudeDestino,
+        longitudeDestino);
+
+    //Converter para KM
+    double distanciaKM = distanciaEmMetros / 1000;
+
+    //8 é o valor cobrado por KM
+    double valorViagem = distanciaKM * 8;
+
+    //Formatar o valor cobrado por KM
+    var f = NumberFormat('#,##0.00', 'pt_BR');
+    var valorViagemFormatado = f.format( valorViagem );
+
+    _mensagemStatus = "Viagem finalizada";
+    _alterarBotaoPrincipal("Confirmar - R\$ ${valorViagemFormatado}",
+        Colors.lightBlue, () {
+      _confirmarCorrida();
+    });
+
+  }
+
+  _confirmarCorrida() {
 
   }
   _statusEmViagem() {
